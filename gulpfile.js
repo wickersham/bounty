6ccf92dceaa21c0c5c086b9d5e883a65f8b817c7
[Sean Krauth] <[spkrauth@gmail.com]>
2015-11-10 18:32:48 -0500

    fleshed out minification of js and css, now runs as one task

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,41 @@ var gulp = require("gulp");
 var jshint = require("gulp-jshint");
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
+var mcss = require("gulp-mcss");
 
 gulp.task("default", function(){
 //This is where code for default task goes
 });
 
 gulp.task("hint", function(){
-    return gulp.src(["public/js/**/*.js", "!public/js/**/*.min.js"])
+    gulp.src(["public/js/**/*.js", "!public/js/**/*.min.js"])
         .pipe(jshint())
         .pipe(jshint.reporter("default"))
 });
 
-gulp.task("js", ["hint"], function(){
+gulp.task("js-to-build", function(){
+    return gulp.src("public/js/**/*.min.js")
+        .pipe(gulp.dest("build/js"))
+});
+
+gulp.task("css-to-build", function(){
+    return gulp.src("public/css/**/*.min.css")
+        .pipe(gulp.dest("build/css"))
+});
+
+gulp.task("js", ["hint", "js-to-build"], function(){
     return gulp.src(["public/js/**/*.js", "!public/js/**/*.min.js"])
         .pipe(uglify())
         .pipe(concat("app.js"))
         .pipe(gulp.dest("build/js"))
 });
 
-gulp.task("build", ["js"])
+gulp.task("css", ["css-to-build"], function(){
+    return gulp.src(["public/css/**/*.css", "!public/css/**/*.min.css"])
+        .pipe(mcss())
+        .pipe(concat("main.css"))
+        .pipe(gulp.dest("build/css"))
+
+});
+
+gulp.task("build", ["js", "css"])
