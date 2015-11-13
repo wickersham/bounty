8e7c9659cc24eea0deaada24e66298eaac6decf7
[Sean Krauth] <[spkrauth@gmail.com]>
2015-11-12 19:28:06 -0500

    first outline for server tests, minor cleanup

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,7 +1,39 @@
 process.env.NODE_ENV = "test"
+
+//So does mocha play nice with angular, lets find out.
+angular.module("ngPrograms", []);
+
 describe("Server-side test", function(){
-    it("should send home page on get request", function(){
-        //write a get request to server, success if it responds with index.html
+    it("Returns index.html on get '/'", function(){
         
+    });
+    
+    it("Returns {} on get '/api/programs'", function(){
+angular.module("ngGet")
+        .controller("GetController", function(){
+        $scope.getProgram = function(){
+            $http.get("/api/programs")
+              .then(function(response){
+                  $scope.programs = response.data;
+                  console.log($scope.programs);
+            });
+          };
+        });
+    });
+    
+    it("Return a json on post '/api/programs'", function(){
+angular.module("ngPost")
+        .controller("PostController", function($scope, $http){
+          $scope.programData = {};
+
+          $scope.createProgram = function(){
+            console.log("Pancakes");
+            $http.post("/api/programs", $scope.programData)
+              .then(function(response){
+                console.log(response);
+                $scope.programData = {};
+              });
+          };
+    
     });
 });
