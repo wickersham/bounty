263de2490bad58aea735aac4633b12f70b3d2135
nnnoel <ncolon20@gmail.com>
2015-11-16 18:23:02 -0500

    Fixed handlebars. Styled publicView.html.

--- public/js/template.js
+++ public/js/template.js
@@ -2,10 +2,19 @@
 
 var response = [
       {
-        name: "me"
+        school: "Bradford Tech Center"
       },
       {
-        name: "you"
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      },
+      {
+        phone: "555-555-5555"
+      },
+      {
+        location: "Gainesville, FL"
+      },
+      {
+        moreInfo: "More info button with #myModel here"
       }
     ];
 
@@ -20,5 +29,49 @@ $.handlebars({
 $('#viewpage').render('viewpage', {
     employer: 'Hello',
     field2: 'world!',
-    programs: 'response'
+    programs: response
 });
+
+// Dropdowns on viewpage; publicView.html
+  function DropDown(el) {
+    this.dd = el;
+    this.placeholder = this.dd.children('span');
+    this.opts = this.dd.find('ul.dropdown > li');
+    this.val = '';
+    this.index = -1;
+    this.initEvents();
+  }
+  DropDown.prototype = {
+    initEvents : function() {
+      var obj = this;
+
+      obj.dd.on('click', function(event){
+        $(this).toggleClass('active');
+        return false;
+      });
+
+      obj.opts.on('click',function(){
+        var opt = $(this);
+        obj.val = opt.text();
+        obj.index = opt.index();
+        obj.placeholder.text(obj.val);
+      });
+    },
+    getValue : function() {
+      return this.val;
+    },
+    getIndex : function() {
+      return this.index;
+    }
+  }
+
+  $(function() {
+
+    var dd = new DropDown( $('#dd') );
+
+    $(document).click(function() {
+      // all dropdowns
+      $('.wrapper-dropdown-3').removeClass('active');
+    });
+
+  });
