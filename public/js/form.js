10f3d0cf17b2378acf31663ab2457e95c9a766f0
[Sean Krauth] <[spkrauth@gmail.com]>
2015-11-16 21:04:36 -0500

    updated form.js to match new schema

--- public/js/form.js
+++ public/js/form.js
@@ -22,22 +22,35 @@ $(document).ready(function(){
 //building object from form.html's inputs
         var formObject = {};
         formObject.programName = $( "#programName" ).val();
+        formObject.county = $( "#county" ).val();
+        formObject.contactName = $( "#contactName" ).val();
+        formObject.email = $( "#email" ).val();
+        formObject.phoneNumber = $( "#phoneNumber" ).val();
+        formObject.cost = $( "#cost" ).val();
+        formObject.certificate = $( "#certificate" ).val();
         formObject.timeline = $( "#timeline" ).val();
+        formObject.timeOfDay = $( "#timeOfDay" ).val();
         formObject.meals = $( "#meals" ).val();
         formObject.affiliation = $( "#affiliation" ).val();
         formObject.otherBenefits = $( "#otherBenefits" ).val();
+        formObject.ageMin = $( "#ageMin" ).val();
+        formObject.ageMax = $( "#ageMax" ).val();
         formObject.pastSuccess = $( "#pastSuccess" ).val();
+        formObject.pastParticipants = $( "#pastParticipants" ).val();
         formObject.wordOut = $( "#wordOut" ).val();
         formObject.funded = $( "#funded" ).val();
-        formObject.numParticipants = $( "#numParticipants" ).val();
+        formObject.impediments = $( "#impediments" ).val();
+        formObject.avgNumParticip = $( "#avgNumParticip" ).val();
         formObject.description = $( "#description" ).val();
         formObject.programUrl = $( "#programUrl" ).val();
         formObject.location = $( "#location" ).val();
         formObject.partners = $( "#partners" ).val();
+        formObject.servePop = $( "#servePop" ).val();
         formObject.scholarships = $( "#scholarships" ).val();
         formObject.parentsInvolved = $( "#parentsInvolved" ).val();
         formObject.measureSuccess = $( "#measureSuccess" ).val();
         formObject.NCFLFunded = $( "#NCFLFunded" ).val();
+        formObject.bringDayOne = $( "#bringDayOne" ).val();
         formObject.inPerson = $( "#inPerson" ).val();
         console.log(formObject);
 
