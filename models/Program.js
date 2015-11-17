4ec6c82f1c36f36faa83b5c50206ffa2ecaa67f0
Fistbutter <djlehnen@gmail.com>
2015-11-16 20:57:29 -0500

    cleaned up some code, indents, lining up, etc

--- models/Program.js
+++ models/Program.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-//user models
+//program model
 var programSchema = new Schema({
     programName: {
                     type: String,
