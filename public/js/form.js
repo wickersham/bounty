8fc7474bd161a67b5a959e2ea082f35ebe678d6d
[Sean Krauth] <[spkrauth@gmail.com]>
2015-11-16 19:42:43 -0500

    wrote post route for form

--- public/js/form.js
+++ public/js/form.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     console.log("working!");
+//Using .submit to handle the form and link the submit button to an action.
     $( "#programForm" ).submit(function( event ){
         console.log("submitted!");
+//building object from form.html's inputs
         var formObject = {};
         formObject.programName = $( "#programName" ).val();
         formObject.timeline = $( "#timeline" ).val();
@@ -23,6 +25,16 @@ $(document).ready(function(){
         formObject.inPerson = $( "#inPerson" ).val();
         console.log(formObject);
 //This will stop the submit funtion from refreshing page, possibly for missing form pieces.
+        $.post("/api/programs", formObject, function(){
+            console.log("POSTING!!");
+        })
+            .done(function(){
+                console.log("success");
+            })
+//If this works it needs to inheret event.preventDefault()
+            .fail(function(){
+                console.log("fail");
+            });
         event.preventDefault();
     });
 
