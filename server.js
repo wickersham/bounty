2a9411adbd5adf64f17d89d61f79e1588554fdd0
wickersham <wickersham90@gmail.com>
2015-11-16 19:13:19 -0500

    minor changes

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ server.get("/api/programs/time/:timeline", function(req, res){
 //   });
 // });
 
+//refers to our mongoose schema
 server.post("/api/programs", function(req, res){
     console.log(req.body);
     var program = new Program({
