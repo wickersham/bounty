3b8758d625cbc1b7d20d6bc5f76617a4b7cfd772
Antaeusnemty <gamingemail007@gmail.com>
2015-11-17 21:03:13 -0500

    changed ids for each dropdown made new variables in program list js fixed nav links on forms html deleted module

--- public/js/program.list.js
+++ public/js/program.list.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     var programList = [];
-    var $test = $("#test");
     //These are references to the select drop down boxes for search functionality
     var $cost = $("#cost");
-    var $county = $("#county");
+    var $location = $("#location");
     var $duration = $("#duration");
     var $age = $("#age");
 
@@ -12,10 +11,11 @@ $(document).ready(function(){
     //This function will obtain the programs collection from the server
     function getPrograms(){
         console.log("before");
-        console.log($cost.val());
-        console.log($county.val());
-        console.log($duration.val());
-        console.log($age.val());
+      var $cost = $("#cost span")[0].innerText;
+      var $location = $("#location span")[0].innerText;
+      var $duration = $("#duration span")[0].innerText;
+      var $age = $("#age span")[0].innerText;
+
         var options = {
             method: "GET",
             url: "/api/programs",
@@ -25,7 +25,6 @@ $(document).ready(function(){
         $.ajax(options)
          .done(function(data){
             console.log(data[0]);
-            $test.html(data[0].programName);
             programList = data;
         })
          .error(function(err){
