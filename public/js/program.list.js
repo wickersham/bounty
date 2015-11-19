d4d410774914e1d38b32e6e0010372b800826b7c
Fistbutter <djlehnen@gmail.com>
2015-11-19 18:51:37 -0500

    modals are populated with data, but only the first entry in database

--- public/js/program.list.js
+++ public/js/program.list.js
@@ -8,20 +8,11 @@ $(document).ready(function(){
 
     var programList = [];
     //These are references to the select drop down boxes for search functionality
-    var $cost = $("#cost");
-    var $location = $("#location");
-    var $duration = $("#duration");
-    var $age = $("#age");
 
     $("#search").on('click', getPrograms);
 
     //This function will obtain the programs collection from the server
     function getPrograms(){
-        console.log("before");
-      var $cost = $("#cost span")[0].innerText;
-      var $location = $("#location span")[0].innerText;
-      var $duration = $("#duration span")[0].innerText;
-      var $age = $("#age span")[0].innerText;
 
         var options = {
             method: "GET",
