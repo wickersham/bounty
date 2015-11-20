75d12b9caaf7f38038024993e7b54713034daa52
Fistbutter <djlehnen@gmail.com>
2015-11-19 20:00:55 -0500

    each entry has a button that will reveal all relevant info for the chosen entry

--- public/js/program.list.js
+++ public/js/program.list.js
@@ -9,7 +9,9 @@ $(document).ready(function(){
     var programList = [];
     //These are references to the select drop down boxes for search functionality
 
-
+    $("#viewpage").on("click", "button", function(){
+      $(this).siblings(".more-info").fadeIn();
+    });
     $("#search").on('click', getPrograms);
 
     //This function will obtain the programs collection from the server
@@ -23,11 +25,12 @@ $(document).ready(function(){
 
         $.ajax(options)
          .done(function(data){
-            console.log(data[0]);
             // now this will fetch <path/to/templates/content.hbs>
+
             $('#viewpage').render('viewpage', {
 
                 programs: data
+
             });
         })
          .error(function(err){
